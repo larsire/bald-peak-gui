9ca24dd88d8f268bd458cb1c4f803e9bdaafdc62
Morty <dj@kazan.com.pl>
2019-05-07 22:36:33 +0200

    Added icons, small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component, Input } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: [
+    "./app.component.css"
+  ]
 })
 export class AppComponent {
   title = "angular-electron";
